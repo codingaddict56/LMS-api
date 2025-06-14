9204c64f82421c44e8d7b465b92998d4a5c49677
jaggannatha_r <chaithra.jagannatha_rao_telkar@mail.schwarz>
2025-06-14 20:00:15 +0200

    minor changes

--- src/main/java/com/example/LMS_api/controller/BookController.java
+++ src/main/java/com/example/LMS_api/controller/BookController.java
@@ -5,11 +5,13 @@ import com.example.LMS_api.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService service;
